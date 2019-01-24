9fbb6d59a4cfbd5ac4af63a2326b4f8d33d524b6
ChangYong <lcy9002@naver.com>
2019-01-24 15:15:48 +0900

    Delete unused variable
    사용하지않는 변수 삭제, 변수 네이밍 컨벤션 수정

--- src/main/java/makeshop/URL.java
+++ src/main/java/makeshop/URL.java
@@ -4,14 +4,13 @@ public class URL {
 
   private String host;
   private String subURL;
-  private String combineUrl;
 
   public URL(String parentUrl, String subUrl) {
     this.host = parentUrl.substring(0, parentUrl.length()-1);
     this.subURL = subUrl;
   }
 
-  public String getCombineUrl() {
+  public String getCombineURL() {
     return host + subURL;
   }
 

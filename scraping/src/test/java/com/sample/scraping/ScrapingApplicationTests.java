f7af85f57d0d0f3b06b4950f61a6cfa766ac550b
msj <msjung@wemakeprice.com>
2019-02-01 11:31:46 +0900

    크롤링, 스크랩핑 분리

---
+++ scraping/src/test/java/com/sample/scraping/ScrapingApplicationTests.java
@@ -0,0 +1,17 @@
+package com.sample.scraping;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ScrapingApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
+
